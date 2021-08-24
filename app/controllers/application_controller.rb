9eaf3fb5bee00b436c49fa593b5d84ed2d8d2c0d
KucharskiPiotr <piotr-kucharski@outlook.com>
2021-08-25 01:00:20 +0200

    Move searches to separate pages, XSS-Reflected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,22 @@ class ApplicationController < ActionController::Base
     end
 
   def search
-    return unless params[:title].present?
-
-    # To exploit that query run eg. "' UNION SELECT login as title, password as body FROM USERS where '1%'='1"
-    @articles = ActiveRecord::Base.connection.execute("SELECT title FROM articles WHERE title LIKE '%#{params[:title]}%'")
-    puts 'Returned articles', @articles
-    render "index"
+    if params[:title].present?
+        # To exploit that query run eg. "' UNION SELECT login as title, password as body FROM USERS where '1%'='1"
+        @articles = ActiveRecord::Base.connection.execute("SELECT title FROM articles WHERE title LIKE '%#{params[:title]}%'")
+        @query = params[:title]
+    end
+    
+    render action: :search
   end
 
   def safe_search
-    return unless params[:title].present?
+    if params[:title].present?
+        @articles = Article.where('title LIKE ?', "%#{params[:title]}%")
+        @query = params[:title]
+    end
 
-    @articles = Article.where('title LIKE ?', "%#{params[:title]}%")
-    render action: :index
+    render action: :search_safe
   end
 
 end
